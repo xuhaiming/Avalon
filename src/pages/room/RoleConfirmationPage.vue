8be1901e85378856bec16b024df283b8b5212605
Zhihui Yan <Zhihui Yan>
2016-11-26 18:18:20 +0800

    optimise page for confirmation page

--- src/pages/room/RoleConfirmationPage.vue
+++ src/pages/room/RoleConfirmationPage.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <h4>You are {{ currentUser.role }}</h4>
-    <h5>You can see</h5>
-    <p v-for="personCanSee in getPlayersCanSee()">{{ personCanSee.name }}</p>
-    <button @click="conformRole">Confirm</button>
-    <div v-for="player in room.players">
-      <span>{{ player.name }}: </span>
-      <span>{{ getPlayerStatus(player.status) }}</span>
+  <div class="role-confirmation-page container">
+    <div class="row">
+      <p class="col s12 text-float">
+        You are <b>{{ currentUser.role }}</b>
+      </p>
+    </div>
+
+    <div class="row" v-if="getPlayersCanSee().length != 0">
+      <ul class="collection with-header col s12">
+        <li class="collection-header"><b>You can see {{ getRolesCanSee() }}</b></li>
+        <li class="collection-item" v-for="personCanSee in getPlayersCanSee()">
+          {{ personCanSee.name }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="row">
+      <ul class="collection with-header col s12">
+        <li class="collection-header"><b>Please confirm the info for you</b></li>
+        <li class="collection-item" v-for="player in room.players">
+          {{ player.name }}:
+          <span class="pull-right">{{ getPlayerStatus(player.status) }}</span>  
+        </li>
+      </ul>
+    </div>
+
+    <div class="row">
+      <button @click="conformRole" class="col s12 m6 offset-m3 btn">Confirm</button>    
     </div>
   </div>
 </template>
@@ -36,9 +56,14 @@ export default {
 
       return statusMapping[status]
     },
+    getRolesCanSee() {
+      const rolesCanSee = roles[this.currentUser.role].canSee
+      
+      return rolesCanSee;
+    },
     getPlayersCanSee() {
       const players = this.room.players
-      const rolesCanSee = roles[this.currentUser.role].canSee
+      const rolesCanSee = this.getRolesCanSee();
       const playersCanSee = _.filter(players, player => _.includes(rolesCanSee, player.role))
 
       return playersCanSee
