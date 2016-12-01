760e6d3a819d084736d5c78b9b8ea6fc11951459
Haiming Xu <Haiming_Xu@epam.com>
2016-12-01 11:25:31 +0800

    Use client side cookie to store username

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,6 @@ export default {
     io,
     user,    
     room,
-    rooms
+    rooms,
   }
 }
--- src/store/user.js
+++ src/store/user.js
@@ -1,9 +1,12 @@
+import Cookies from 'cookies-js'
+
 export default {
   state: {
-      name: undefined
+      name: Cookies.get('username')
   },
   actions: {
     user_setName({ commit, state }, name) {
+      Cookies.set('username', name)
       state.name = name
     }
   }
